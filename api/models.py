0885636fc997e290e765962356ea6d616f5c244c
kihagama <ismaelkihagama@gmail.com>
2025-12-28 07:24:46 +0300

    Added the product model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=50,choices=[
+        ('Fruits', 'Fruits'),
+        ('Vegetables', 'Vegetables'),
+        ('Animals', 'Animals'),
+        ("Birds", "Birds"),
+    ]),
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
