136c1cbd0aaea9e96d0cd3ba42da9a2f187d2688
kihagama <ismaelkihagama@gmail.com>
2025-12-28 07:53:11 +0300

    configured the Product model and added some products to the database view the admin panel

--- api/models.py
+++ api/models.py
@@ -5,12 +5,13 @@ class product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.CharField(max_length=50,choices=[
+    category = models.CharField(max_length=20, choices=[
         ('Fruits', 'Fruits'),
         ('Vegetables', 'Vegetables'),
         ('Animals', 'Animals'),
-        ("Birds", "Birds"),
-    ]),
+        ("Birds", "Birds")
+    ], blank=True, null=True)
+    Image = models.ImageField(upload_to='products/',blank=True, null=True)
     
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
